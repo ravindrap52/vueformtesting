f57799cb642d0534c33efbbdb747a1471c848319
Ravi <c-ravi@its-dost.com>
2020-04-15 03:37:36 +0530

    unit test casewith vuex error fixed

--- tests/unit/app.spec.js
+++ tests/unit/app.spec.js
@@ -2,12 +2,13 @@ import { shallowMount, createLocalVue } from "@vue/test-utils";
 import Vuex from "vuex";
 import App from "@/App.vue";
 import Login from "@/views/Login.vue";
+import flushPromises from "flush-promises";
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
 
 describe("App.vue", () => {
-  it("trigger event", () => {
+  it("trigger event", async () => {
     const storeConfig = {
       actions: {
         userLogin: jest.fn()
@@ -15,6 +16,7 @@ describe("App.vue", () => {
       state: {}
     };
     const store = new Vuex.Store(storeConfig);
+    store.dispatch = jest.fn(() => Promise.resolve());
     const wrapper = shallowMount(App, {
       localVue,
       attachToDocument: true,
@@ -23,5 +25,8 @@ describe("App.vue", () => {
       }
     });
     wrapper.find(Login).vm.$emit("form-submitted");
+    await flushPromises();
+    let data;
+    expect(wrapper.vm.$store.dispatch).toHaveBeenCalledWith("userLogin", data);
   });
 });
